a34a10912be2064d0a1b23ba4a69c302aea8431c
angie <angelaclangat@gmai.com>
2024-09-09 16:38:05 +0300

    Orgenizes Bears Lab

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 10, 'M', 'brown', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 4, 'M', 'black', 'aggressive', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 6, 'F', 'brown', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 9, 'M', 'brown', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 7, 'F', 'black', 'friendly', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 8, 'M', 'green', 'grumpy', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 5, 'F', 'white', 'playful', 1);
+
+-- Inserting the unnamed bear with a NULL name
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 3, 'F', 'black', 'timid', 1);
